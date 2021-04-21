76b4527f3d7bb9c854fdaf7293c1332876bdc059
AliasDinesh <302115525@student.rocmondriaan.nl>
2021-04-21 12:34:07 +0200

    Updated Theme Switcher

--- js/main.js
+++ js/main.js
@@ -2,23 +2,29 @@ const theme = document.querySelector(".theme-button");
 
 function changeHandler() {
     const elements = document.querySelectorAll(".themed");
+    const cardTheme = document.querySelectorAll(".card-theme");
 
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i];
-        element.classList.toggle("dark");
-
-        if (theme.innerHTML == "Theme: Light") {
-            theme.innerHTML = "Theme: Dark";
-        }
-        else theme.innerHTML = "Theme: Light";        
+        element.classList.toggle("dark");  
     }
+
+    for (let j = 0; j < cardTheme.length; j++) {
+        const cardEelemnt = cardTheme[j];
+        cardEelemnt.classList.toggle("light-grey");
+    }
+
+    if (theme.innerHTML == "Theme: Light") {
+        theme.innerHTML = "Theme: Dark";
+    }
+    else theme.innerHTML = "Theme: Light";
 }
 
 theme.addEventListener("click", changeHandler);
 
 setTimeout(function() {
     const current = new Date().getHours();
-    if (current >= 2) {
+    if (current >= 12) {
         changeHandler();
     }
 },0000);
