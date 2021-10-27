224ddfeed90a1b4231252b1fdf036c1a2b51ad8a
AliasDinesh <302115525@student.rocmondriaan.nl>
2021-10-27 22:09:48 +0200

    Changed project.html to project.php and added a contact form at the contact.html

---
+++ php/contactsform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subjet = $_POST['subject'];
+    $mailForm = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = '302115525@student.rocmondriaan.nl';
+    $headers  = "From: " . $mailForm;
+    $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: ../contact.html?mailsend");
+}
